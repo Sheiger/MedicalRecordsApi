35ae7ee0b86e4f49abf39a68e5d20c35065b0670
emerson quisp <emersonquispe2808@gmail.com>
2026-08-08 18:24:56 -0500

    implementacion de auth controller

--- src/main/java/com/sheigert/medical_records_api/controller/AuthController.java
+++ src/main/java/com/sheigert/medical_records_api/controller/AuthController.java
@@ -1,4 +1,34 @@
 package com.sheigert.medical_records_api.controller;
 
+import com.sheigert.medical_records_api.dto.request.LoginRequest;
+import com.sheigert.medical_records_api.dto.request.RegisterRequest;
+import com.sheigert.medical_records_api.dto.response.AuthResponse;
+import com.sheigert.medical_records_api.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request) {
+        AuthResponse response = userService.register(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
+        AuthResponse response = userService.login(request);
+        return ResponseEntity.ok(response);
+    }
 }
