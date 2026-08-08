f0ba21203d7b64e520b3b851a1b48bbd09f0be8a
emerson quisp <emersonquispe2808@gmail.com>
2026-08-08 12:58:18 -0500

    implementacion de user repository

--- src/main/java/com/sheigert/medical_records_api/repository/UserRepository.java
+++ src/main/java/com/sheigert/medical_records_api/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.sheigert.medical_records_api.repository;
 import com.sheigert.medical_records_api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
