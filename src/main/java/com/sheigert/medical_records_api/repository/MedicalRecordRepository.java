4a4bc2b35b420ec55781feb6fb1508aaa49317de
emerson quisp <emersonquispe2808@gmail.com>
2026-08-08 13:10:57 -0500

    implementacion de medical record repository

--- src/main/java/com/sheigert/medical_records_api/repository/MedicalRecordRepository.java
+++ src/main/java/com/sheigert/medical_records_api/repository/MedicalRecordRepository.java
@@ -1,4 +1,13 @@
 package com.sheigert.medical_records_api.repository;
 
-public class MedicalRecordRepository {
+import com.sheigert.medical_records_api.entity.MedicalRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
+    Optional<MedicalRecord> findByAppointmentId(Long appointmentId);
+
+    List<MedicalRecord> findByActiveTrue();
 }
