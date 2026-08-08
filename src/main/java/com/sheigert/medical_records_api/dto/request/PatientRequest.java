da84126f3aa6afc6d92051dfc174431416965023
emerson quisp <emersonquispe2808@gmail.com>
2026-08-08 18:29:07 -0500

    implementacion de dto de patient

---
+++ src/main/java/com/sheigert/medical_records_api/dto/request/PatientRequest.java
@@ -0,0 +1,22 @@
+package com.sheigert.medical_records_api.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class PatientRequest {
+
+    @NotNull(message = "User ID is required")
+    private Long userId;
+
+    @NotBlank(message = "Document id is required")
+    private String documentId;
+
+    @NotNull(message = "Borth date id required")
+    private LocalDate birthDate;
+
+    private String phone;
+}
