1ce3b70dd6c87ea4d322c044fdc439772389e586
emerson quisp <emersonquispe2808@gmail.com>
2026-08-09 12:56:50 -0500

    configuracion de swagger

--- src/main/java/com/sheigert/medical_records_api/config/SecurityConfig.java
+++ src/main/java/com/sheigert/medical_records_api/config/SecurityConfig.java
@@ -51,8 +51,8 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/api/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
